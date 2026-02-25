c250966ffdca20ea8283c3ee8de4ac9896e65752
MaximeD <one.maximeduboispro@gmail.com>
2026-02-25 08:54:13 +0100

    add routes

---
+++ server/routes/artisans.js
@@ -0,0 +1,11 @@
+// Routes des artisans
+
+const express = require("express");
+const router = express.Router();
+const { getAllArtisans, getTopArtisans, getArtisanById } = require("../controllers/artisanController");
+
+router.get("/top", getTopArtisans);   // Avant /:id !
+router.get("/", getAllArtisans);
+router.get("/:id", getArtisanById);
+
+module.exports = router;
